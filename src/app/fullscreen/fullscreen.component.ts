b21c3ec927d5dd970322eec88eccfb7bf95e8313
Hasan-KHALID <khalidhasan842@gmail.com>
2022-09-11 17:13:26 +0600

    FULL sevreen 2

--- src/app/fullscreen/fullscreen.component.ts
+++ src/app/fullscreen/fullscreen.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { GalleryService } from '../gallery/gallery.service';
+// import { GalleryService } from '../gallery/gallery.service';
 import { Image } from '../gallery/image.module';
 
 @Component({
@@ -12,12 +12,12 @@ export class FullscreenComponent implements OnInit{
   imageName!:string;
   imagePath!: string
 
-  constructor(private galleryService: GalleryService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.image = this.galleryService.images;
-    this.imageName = this.galleryService.names
-    this.imagePath = this.galleryService.imageUrl
+    // this.image = this.galleryService.images;
+    // this.imageName = this.galleryService.names
+    // this.imagePath = this.galleryService.imageUrl
 
     console.log(this.imageName)
 
